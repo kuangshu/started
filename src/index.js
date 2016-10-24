2b08a40a93a512477c87fe339d819b0f42f755a2
kuangshu <252019869@qq.com>
2016-10-24 10:51:14 +0800

    1.01
    add index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
+import {
+	Router,
+	Route,
+	hashHistory,
+	IndexRoute
+} from 'react-router';
 
+import Home from './components/Home/Home'
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render((
+	<Router history={hashHistory}>
+		<Route path="/" component={App}>
+			<IndexRoute component={Home}/>
+		</Route>
+	</Router>
+), document.getElementById('app'));
