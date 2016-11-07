7e484c3cd262d6751a8b80e7362cb19d15f8394f
kuangshu <252019869@qq.com>
2016-11-07 16:12:34 +0800

    11-07
    16:12

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,8 +7,9 @@ class AppComponent extends React.Component {
 	render() {
 		return (
 			<div className="index">
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+				<div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+				{this.props.children}
+			</div>
 		);
 	}
 }
