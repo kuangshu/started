7b7b733dff3bded44c3d8d44027228aff9c467f7
kuangshu <252019869@qq.com>
2016-11-08 11:22:10 +0800

    11-08
    11:22

--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './components/Main';
 import {
 	Router,
 	Route,
-	hashHistory,
+	useRouterHistory,
 	IndexRoute
 } from 'react-router';
+import { createHashHistory } from 'history';
+
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 import Home from './components/Home/Home'
 // Render the main component into the dom
 ReactDOM.render((
-	<Router history={hashHistory}>
+	<Router history={appHistory}>
 		<Route path="/" component={App}>
 			<IndexRoute component={Home}/>
 		</Route>
