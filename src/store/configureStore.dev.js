b77a4b5ec09fd13ad938850ebfbcb8f9b4a9fd42
kuangshu <252019869@qq.com>
2016-12-06 17:30:04 +0800

    删除middleware

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import createLogger from 'redux-logger';
-import {api} from '../middleware/api';
+//import {api} from '../middleware/api';
 import rootReducer from 'reducers';
 
 const configureStore = preloadedState => {
@@ -9,7 +9,7 @@ const configureStore = preloadedState => {
         rootReducer,
         preloadedState,
         compose(
-            applyMiddleware(thunk, api, createLogger())
+            applyMiddleware(thunk, createLogger())
         )
     );
 
