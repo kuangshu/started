769debe2c869c804d4ddad1e8bd15d3021741d2a
kuangshu <252019869@qq.com>
2017-02-16 09:50:00 +0800

    修改#删除action type

--- src/components/Home/reducer.js
+++ src/components/Home/reducer.js
@@ -1,11 +1,10 @@
 import merge from 'lodash/merge'
 import { routerReducer as routing } from 'react-router-redux'
 import { combineReducers } from 'redux'
-import * as types from '../actions/types';
 
 const base = (state = '', action) => {
 	switch (action.type) {
-		case types.FILTER:
+		case 'FILTER':
 			return merge(state, {
 				filter: action.filter
 			});
