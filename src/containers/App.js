01c8711444586498e7ceea3cf61595c2d2295f24
kuangshu <252019869@qq.com>
2017-05-17 16:51:28 +0800

    修正图片地址

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import React, { PropTypes } from 'react';
-import logo from 'images/logo.svg';
+import logo from 'images/svg/logo.svg';
 
 function mapStateToProps(state) {
     return { state: state };
